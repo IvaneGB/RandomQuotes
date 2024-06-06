b2bd3d9ee4be9edaa1f2717b766ac2340fc585fc
IvaneGB <ivaneddugonza@gmail.com>
2024-06-06 14:37:14 -0500

    Se soluciono el problema de que la frase sobresalia sobre la caja que lo contenia,además se le agrego los tags que le corresponden a cada frase

--- js/script.js
+++ js/script.js
@@ -1,45 +1,44 @@
-// let frase = ''
 
-// fetch('https://api.quotable.io/random')
-//   .then(response => response.json())
-//   .then(data => {
-//     console.log(`${data.content} —${data.author} —${data.tags}`)
-//     frase = `${data.content}`
-//   })
-//   .catch(error => {
-//     console.log(error)
-//   })
-
-//   console.log(frase);
 
 let quote = document.getElementById('quote');
 let author = document.getElementById('author');
+let etiquetas = document.getElementById('tags');
 let bntNewQuote = document.getElementById('new-quote');
 let frase = '';
 let autor = '';
-let tags = '';
+let tags = [];
 
 async function fetchQuote() {
-  try {
-    const response = await fetch('https://api.quotable.io/random');
-    const data = await response.json();
-    frase = `${data.content}`;
-    autor = `${data.author}`;
-    tags = `${data.tags}`;
-    
-    return { frase, autor, tags };
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        const response = await fetch('https://api.quotable.io/random');
+        const data = await response.json();
+        frase = `${data.content}`;
+        autor = `${data.author}`;
+        // tags = `${data.tags}`;
+        tags = data.tags;
+
+        return { frase, autor, tags };
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 async function displayQuote() {
-  await fetchQuote();
-  console.log("frase: "+frase,"\nautor: "+ autor,"\netiquetas: "+ tags); // Esto se ejecutará después de que fetchQuote se complete
-    quote.innerHTML = '"'+frase+'"';
+    await fetchQuote();
+    quote.innerHTML = '"' + frase + '"';
     author.innerHTML = autor;
+
+    // Limpiar el contenido anterior
+    etiquetas.innerHTML = '';
+
+    // Crear elementos <p> para cada tag y añadirlos al contenedor
+    tags.forEach(tag => {
+        const tagElement = document.createElement('p');
+        tagElement.textContent = tag;
+        etiquetas.appendChild(tagElement);
+    });
+    console.log(tags);
 }
 
 
 bntNewQuote.addEventListener('click', displayQuote);
-// displayQuote();
